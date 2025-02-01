0d1f98f0c0c1d30a0fcd33bf3ae7343a9ee51e2d
andrerwolff <andre.r.wolff@gmail.com>
2025-02-01 14:41:31 -0700

    update 2/1

--- Triplo_Home.py
+++ Triplo_Home.py
@@ -8,3 +8,19 @@ st.set_page_config(
 
 st.write("# Welcome to Triplo! 👋")
 st.write(lorem.paragraph())
+
+st.sidebar.button("Manage Account")
+st.sidebar.button("Prefrences")
+
+project_list = ["*Lafayette WRF Improvements*", "*NWC - CSU Spur*"]
+
+c1 = st.container(border=True)
+c1.header("Project List")
+
+for project in project_list:
+    expander = c1.expander(project)
+    expander.write(lorem.sentence())
+    expander.button("Go to Project", key=project)
+
+c2 = st.container(border=True)
+c2.button("Start New Project")
